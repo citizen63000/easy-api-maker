a13fbffc6ab8f7ccd9eca02847f2773325fe9b9e
lionel <ngconnected@gmail.com>
2025-03-07 10:52:53 +0100

    php8.3 code style in MakeCrudCommand.php

--- Command/MakeCrudCommand.php
+++ Command/MakeCrudCommand.php
@@ -2,9 +2,7 @@
 
 namespace EasyApiMaker\Command;
 
-use EasyApiMaker\Util\StringUtils\CaseConverter;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -12,7 +10,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 final class MakeCrudCommand extends AbstractMakerCommand
 {
-    protected function configure()
+    protected function configure(): void
     {
         $this
             ->setName(self::$commandPrefix.':crud')
